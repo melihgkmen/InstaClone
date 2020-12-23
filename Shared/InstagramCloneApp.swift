5f144a18386816cc4df233bbbb8a65d7d6083825
melih <gokmen>
2020-12-23 22:30:16 +0300

    Initial Commit

---
+++ Shared/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  Shared
+//
+//  Created by Melih on 23.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
